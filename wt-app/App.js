a8f5cd9096895710631ca02f621ea402ee411751
6210110104 <6210110104@psu.ac.th>
2022-08-25 18:10:18 +0700

    first commit

--- wt-app/App.js
+++ wt-app/App.js
@@ -2,12 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const doIt = () => {
-    console.log("Hello from console")
-  }
   return (
     <View style={styles.container}>
-      <Text onPress={doIt}>Hello World</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
